f152cc48455e5368402f726b2975762b641335dd
lifanova <veralifanovaa@gmail.com>
2021-03-11 15:18:29 +0300

    Project files are updated

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,25 @@
 public class CreditPaymentService {
-    public double calculate(){
-        return 0;
+    private static final int MONTHS = 12;
+
+    public double calculate(double creditSum, int years, double percents) {
+        double resultSum = 0;
+        /**
+         * Аннуитетный платеж = Сумма_кредита * ((i * (1 + i)^n) / (1 + i)^n - 1),
+         *
+         * где
+         i — ставка в процентах по кредиту в месяц,
+         например если кредит под 12% годовых, то получим 12/12/100 = 0.01 или 1 процент в месяц
+         n — срок кредита, обычно в месяцах
+         *
+         * */
+
+        int n = years * MONTHS;
+        double i = percents / MONTHS / 100;
+        double temp = i * Math.pow((1 + i), n) / (Math.pow((1 + i), n) - 1);
+
+        resultSum = creditSum * temp;
+
+
+        return resultSum;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        double result = service.calculate();
-        System.out.println(result);
+        double creditSum = 1_000_000;
+        int creditPeriod = 1; //Годы
+        double percents = 9.99;
+
+        double result = service.calculate(creditSum, creditPeriod, percents);
+        System.out.printf("Срок кредита (годы) %d. Сумма ежемесячного платежа =  %.0f рублей\n", creditPeriod, result);
+
+        creditPeriod = 2;
+
+        result = service.calculate(creditSum, creditPeriod, percents);
+        System.out.printf("Срок кредита (годы) %d. Сумма ежемесячного платежа =  %.0f рублей\n", creditPeriod, result);
+
+        creditPeriod = 3;
+
+        result = service.calculate(creditSum, creditPeriod, percents);
+        System.out.printf("Срок кредита (годы) %d. Сумма ежемесячного платежа =  %.0f рублей\n", creditPeriod, result);
     }
 }
